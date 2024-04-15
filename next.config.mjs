0cf44b0dea85dbd00224cbee46650120130d7c9f
sayad-mallllek <bob@khalilelzein.com>
2024-04-16 01:32:49 +0300

    fix: dependencies and build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
-import withMDX from '@next/mdx'
- 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-}
- 
-export default withMDX(nextConfig)
+};
+
+export default nextConfig;
